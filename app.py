c1dbbb8ca30daa98b3d3e86a9e9c9cbaadabaa57
Alondra Rubio <ar_lark@hotmail.com>
2024-12-17 17:32:41 -0600

    modificación de if's

--- app.py
+++ app.py
@@ -13,15 +13,15 @@ disp_button = st.button('Construir gráfico de dispersión')
 if hist_button: # al hacer clic en el botón escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
 
-# crear un histograma
-fig = px.histogram(car_data, x="odometer")
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
 
-# mostrar un gráfico Plotly interactivo
-st.plotly_chart(fig, use_container_width=True)
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
 
 if disp_button:
     st.write('Creación de gráfico de disperción para el conjunto de datos de venta de coches')
 
-fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+    fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
 
-st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True)
